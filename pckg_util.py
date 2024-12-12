a76aab98722da569d395052c6dc11d6a1e485e55
Mergaliev Ibragim <mer_eng@mail.ru>
2024-12-12 14:03:51 +0300

    Update pckg_util.py

--- pckg_util.py
+++ pckg_util.py
@@ -91,13 +91,13 @@ def check_gpu_and_torch_compatibility():
             if "NVIDIA A100" in output:
                 install_and_import(
                     "torch",
-                    "1.11.0+cu113",
+                    "1.11.0",
                     "-f",
                     "https://download.pytorch.org/whl/torch_stable.html",
                 )
                 install_and_import(
                     "torchvision",
-                    "0.12.0+cu113",
+                    "0.12.0",
                     "-f",
                     "https://download.pytorch.org/whl/torch_stable.html",
                 )
